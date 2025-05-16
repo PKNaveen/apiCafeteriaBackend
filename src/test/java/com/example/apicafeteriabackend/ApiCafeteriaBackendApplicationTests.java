b04653d905e0479c93ebc4c5cdf46627a9ea1c23
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2025-05-16 19:53:43 +0530

    Initial commit

---
+++ src/test/java/com/example/apicafeteriabackend/ApiCafeteriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicafeteriabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCafeteriaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
